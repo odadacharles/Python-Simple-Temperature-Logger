e4b462dd0b1289b714a9e0772ff08bde087e32fe
Charlie.O <odadacharles@gmail.com>
2021-11-10 16:36:52 +0300

    Data for humidity and temperature are successfully separated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import csv
 
 Temperature = ""
+Dataline =[]
 serialPort = serial.Serial(port = "COM7", baudrate=115200,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
@@ -17,12 +18,22 @@ while(1):
     if(serialPort.in_waiting>0):
         f = open('C:/Users/aolch/Documents/Python Simple Temperature Logger/Kisumu-temperature-humidity.csv', 'w')
         writer=csv.writer(f)
-        Time = str(datetime.now())
+        Time = datetime.now()
         #Read data out of the buffer until a carriage return/new line is found and save to the variable "Temperature"
-        Temperature=str(serialPort.readline() )
-        print(Time)
+        T=str(serialPort.readline() )
+        #print(T)
+        Temperature=T[2:7]
+        Humidity = T[8:13]
+        #print(str(Time)+str(Temperature))
+        
         print(Temperature)
-        writer.writerow(Time)
-        writer.writerow(Temperature)
-        f.close()
+        print(Humidity)
+        #Dataline.append(Time)
+        # Dataline.append(Temperature)
+        # Dataline.append(Humidity)
+        # print(Dataline)
+        #writer.writerows(Dataline)
+        #Dataline=[]
+        #writer.writerows(Temperature)
+        #f.close()
 serialPort.close()
